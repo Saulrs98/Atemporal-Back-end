03a60349bc4b10ad760e20b7a5d24b6760f67608
Edson <e9819@outlook.com>
2022-04-02 11:52:28 -0600

    DATA BASE UPDATED

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -4,144 +4,157 @@ CREATE DATABASE atemporal;
 
 use atemporal;
 
-CREATE TABLE usuarios (
-    id_usuario int,
-    id_rol int,
-    es_admin boolean,
-    nombre varchar(255),
-    celular varchar(255),
-    correo_electronico varchar(255),
-    pass varchar(255),
-    auth_google integer,
-    esta_activo boolean,
-    foto_cuenta varchar(255),
-    fecha_creado date,
-    fecha_actualizado date
-);
-
-CREATE TABLE roles (
-    id_rol int,
-    nombre varchar(255),
-    esta_activo boolean,
-    fecha_creado date,
-    fecha_actualizado date
-);
-
-CREATE TABLE permisos (
-    id_permiso int,
-    nombre varchar(255),
-    esta_activo boolean,
-    fecha_creado date,
-    fecha_actualizado date
-);
-
 CREATE TABLE roles_permisos (
-    id_rol int,
-    id_permiso int,
-    esta_activo boolean,
-    fecha_creado date,
+    id_rol int NOT NULL,
+    id_permiso int NOT NULL,
+    esta_activo boolean NOT NULL,
+    fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fecha_actualizado date
 );
 
-CREATE TABLE eventos (
-    id_evento int,
-    nombre_organizador varchar(255),
-    celular_organizador_principal varchar(255),
-    celular_organizador_secundario varchar(255),
-    nombre_evento varchar(255),
-    fecha_evento date,
-    hora_inicio varchar(255),
-    hora_final varchar(255),
-    ubicacion varchar(255),
-    descripcion varchar(1000),
-    direccion varchar(500),
-    url_video varchar(255),
-    tipo_cobro boolean,
-    foto_evento varchar(255),
-    archivo_itinerario varchar(255),
-    esta_activo boolean,
-    fecha_creado date,
+CREATE TABLE roles_usuarios(
+    id_usuario int NOT NULL,
+    id_rol int NOT NULL,
+    esta_activo boolean NOT NULL,
+    fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fecha_actualizado date
 );
 
 CREATE TABLE usuarios_eventos_crean (
-    id int,
-    id_usuario int,
-    id_evento int,
-    esta_aprobado boolean,
-    esta_activo boolean,
-    fecha_creado date,
+    id int NOT NULL,
+    id_usuario int NOT NULL,
+    id_evento int NOT NULL,
+    esta_aprobado boolean NOT NULL,
+    esta_activo boolean NOT NULL,
+    fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fecha_actualizado date
 );
 
 CREATE TABLE usuarios_eventos_reservan (
-    id int,
-    id_usuario int,
-    id_evento int,
-    asistencia boolean,
-    codigo_qr varchar(255),
-    esta_activo boolean,
-    fecha_creado date,
-    fecha_actualizado date
-);
-
-CREATE TABLE categorias (
-    id_categoria int,
-    nombre varchar(255),
-    esta_activo boolean,
-    fecha_creado date,
-    fecha_actualizado date
-);
-
-CREATE TABLE eventos_categorias (
-    id int,
-    id_categoria int,
-    id_evento int,
-    esta_activo boolean,
-    fecha_creado date,
-    fecha_actualizado date
-);
-
-CREATE TABLE metodos_pago (
-    id_metodo int,
-    nombre varchar(255),
-    esta_activo boolean,
-    fecha_creado date,
+    id int NOT NULL,
+    id_usuario int NOT NULL,
+    id_evento int NOT NULL,
+    asistencia boolean NOT NULL,
+    codigo_qr varchar(255) NOT NULL,
+    esta_activo boolean NOT NULL,
+    fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fecha_actualizado date
 );
 
 CREATE TABLE eventos_metodos (
-    id int,
-    id_metodo int,
-    id_evento int,
-    esta_activo boolean,
-    fecha_creado date,
+    id int NOT NULL,
+    id_metodo int NOT NULL,
+    id_evento int NOT NULL,
+    esta_activo boolean NOT NULL,
+    fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizado date
+);
+
+CREATE TABLE eventos_categorias (
+    id int NOT NULL,
+    id_categoria int NOT NULL,
+    id_evento int NOT NULL,
+    esta_activo boolean NOT NULL,
+    fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizado date
+);
+
+CREATE TABLE usuarios (
+    id_usuario int NOT NULL,
+    id_rol int NOT NULL,
+    es_admin boolean NOT NULL,
+    nombre varchar(255) NOT NULL,
+    celular varchar(255) NOT NULL,
+    correo_electronico varchar(255),
+    pass varchar(255),
+    auth_google integer,
+    esta_activo boolean NOT NULL,
+    foto_cuenta varchar(255),
+    fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizado date
+);
+
+CREATE TABLE roles (
+    id_rol int NOT NULL,
+    nombre varchar(255) NOT NULL,
+    esta_activo boolean NOT NULL,
+    fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizado date
+);
+
+CREATE TABLE permisos (
+    id_permiso int NOT NULL,
+    nombre varchar(255) NOT NULL,
+    esta_activo boolean NOT NULL,
+    fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizado date
+);
+
+CREATE TABLE eventos (
+    id_evento int NOT NULL,
+    nombre_organizador varchar(255) NOT NULL,
+    celular_organizador_principal varchar(255) NOT NULL,
+    celular_organizador_secundario varchar(255) NOT NULL,
+    nombre_evento varchar(255) NOT NULL,
+    fecha_evento date NOT NULL,
+    hora_inicio varchar(255) NOT NULL,
+    hora_final varchar(255) NOT NULL,
+    ubicacion varchar(255) NOT NULL,
+    descripcion varchar(1000) NOT NULL,
+    direccion varchar(500) NOT NULL,
+    url_video varchar(255) NOT NULL,
+    tipo_cobro boolean NOT NULL,
+    foto_evento varchar(255) NOT NULL,
+    archivo_itinerario varchar(255) NOT NULL,
+    esta_activo boolean NOT NULL,
+    fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizado date
+);
+
+CREATE TABLE categorias (
+    id_categoria int NOT NULL,
+    nombre varchar(255) NOT NULL,
+    esta_activo boolean NOT NULL,
+    fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizado date
+);
+
+CREATE TABLE metodos_pago (
+    id_metodo int NOT NULL,
+    nombre varchar(255) NOT NULL,
+    esta_activo boolean NOT NULL,
+    fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fecha_actualizado date
 );
 
 /*AQUELLAS TABLAS SIN LLAVES FORANEAS*/
 
 ALTER TABLE roles
-ADD CONSTRAINT pk_roles PRIMARY KEY (id_rol);
+ADD CONSTRAINT pk_roles PRIMARY KEY (id_rol),
+CHANGE id_rol id_rol int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE permisos
-ADD CONSTRAINT pk_permisos PRIMARY KEY (id_permiso);
+ADD CONSTRAINT pk_permisos PRIMARY KEY (id_permiso),
+CHANGE id_permiso id_permiso int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE categorias
-ADD CONSTRAINT pk_categorias PRIMARY KEY (id_categoria);
+ADD CONSTRAINT pk_categorias PRIMARY KEY (id_categoria),
+CHANGE id_categoria id_categoria int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE metodos_pago
-ADD CONSTRAINT pk_metodos PRIMARY KEY (id_metodo);
+ADD CONSTRAINT pk_metodos PRIMARY KEY (id_metodo),
+CHANGE id_metodo id_metodo int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE eventos
-ADD CONSTRAINT pk_eventos PRIMARY KEY (id_evento);
-
+ADD CONSTRAINT pk_eventos PRIMARY KEY (id_evento),
+CHANGE id_evento id_evento int NOT NULL AUTO_INCREMENT;
 
 /*TABLAS CON LLAVES FORANEAS*/
 
 ALTER TABLE usuarios
 ADD CONSTRAINT pk_usuarios PRIMARY KEY (id_usuario),
-ADD CONSTRAINT fk_rol_usuarios FOREIGN KEY (id_rol) REFERENCES roles(id_rol);
+CHANGE id_usuario id_usuario int NOT NULL AUTO_INCREMENT,
+ADD CONSTRAINT fk_roles_usuarios FOREIGN KEY (id_rol) REFERENCES roles(id_rol);
 
 /*TABLAS CON LLAVES PRIMARIAS COMPUESTAS */
 
@@ -151,32 +164,21 @@ ADD CONSTRAINT fk_permisos FOREIGN KEY (id_permiso) REFERENCES permisos(id_permi
 ADD CONSTRAINT fk_roles_permisos FOREIGN KEY (id_rol) REFERENCES roles(id_rol);
 
 ALTER TABLE usuarios_eventos_crean
-ADD CONSTRAINT pk_usuarios_eventos_crean PRIMARY KEY (id, id_usuario, id_evento),
+ADD CONSTRAINT pk_uec PRIMARY KEY (id, id_usuario, id_evento),
 ADD CONSTRAINT fk_e_crean FOREIGN KEY (id_evento) REFERENCES eventos(id_evento),
 ADD CONSTRAINT fk_u_crean FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario);
 
 ALTER TABLE usuarios_eventos_reservan
-ADD CONSTRAINT pk_usuarios_eventos_reservan PRIMARY KEY (id, id_usuario, id_evento),
+ADD CONSTRAINT pk_uer PRIMARY KEY (id, id_usuario, id_evento),
 ADD CONSTRAINT fk_e_reservan FOREIGN KEY (id_evento) REFERENCES eventos(id_evento),
 ADD CONSTRAINT fk_u_reservan FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario);
 
 ALTER TABLE eventos_categorias
-ADD CONSTRAINT pk_eventos_categorias PRIMARY KEY (id, id_categoria, id_evento),
+ADD CONSTRAINT pk_ec PRIMARY KEY (id, id_categoria, id_evento),
 ADD CONSTRAINT fk_categorias FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria),
 ADD CONSTRAINT fk_eventos_categorias FOREIGN KEY (id_evento) REFERENCES eventos(id_evento);
 
 ALTER TABLE eventos_metodos
-ADD CONSTRAINT pk_eventos_metodos PRIMARY KEY (id, id_metodo, id_evento),
+ADD CONSTRAINT pk_em PRIMARY KEY (id, id_metodo, id_evento),
 ADD CONSTRAINT fk_metodos FOREIGN KEY (id_metodo) REFERENCES metodos_pago(id_metodo),
 ADD CONSTRAINT fk_eventos_metodos FOREIGN KEY (id_evento) REFERENCES eventos(id_evento);
-
-
-
-
-
-
-
-
-
-
-
--- sql/drop_tables.sql
+++ sql/drop_tables.sql
@@ -1,12 +1,15 @@
-DROP TABLE roles_permisos;
-DROP TABLE usuarios_eventos_crean;
-DROP TABLE usuarios_eventos_reservan;
-DROP TABLE eventos_metodos;
-DROP TABLE eventos_categorias;
-DROP TABLE usuarios;
-DROP TABLE roles;
+DROP TABLE IF EXISTS roles_permisos;
+DROP TABLE IF EXISTS roles_usuarios;
+DROP TABLE IF EXISTS usuarios_eventos_crean;
+DROP TABLE IF EXISTS usuarios_eventos_reservan;
+DROP TABLE IF EXISTS eventos_metodos;
+DROP TABLE IF EXISTS eventos_categorias;
+DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS permisos;
 DROP TABLE IF EXISTS eventos;
 DROP TABLE IF EXISTS categorias;
 DROP TABLE IF EXISTS metodos_pago;
+
+
 
