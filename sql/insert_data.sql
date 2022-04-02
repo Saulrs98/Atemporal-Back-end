7820653344ca6eaee6acffdd02652efd48425a05
Daniel Cu Sánchez <danielcu@alternet.com.mx>
2022-04-02 14:10:53 -0600

    ejemplo

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -1,3 +1,7 @@
+-- Usuarios
+
 INSERT INTO `usuarios` (`id_usuario`, `id_rol`, `es_admin`, `nombre`, `celular`, `correo_electronico`, `pass`, `auth_google`, `esta_activo`, `foto_cuenta`, `fecha_creado`, `fecha_actualizado`) VALUES (NULL, NULL, '1', 'Daniel Cu Sánchez', '44812312345', 'danielcu@miempresa.com', 'hash', NULL, '1', NULL, current_timestamp(), NULL);
 
 INSERT INTO `usuarios` (`id_usuario`, `id_rol`, `es_admin`, `nombre`, `celular`, `correo_electronico`, `pass`, `auth_google`, `esta_activo`, `foto_cuenta`, `fecha_creado`, `fecha_actualizado`) VALUES (NULL, NULL, '1', 'Edson Vázquez Cruz', '8333663190', 'correo@electronico.com', 'aaaaaaaaa', NULL, '1', NULL, current_timestamp(), NULL);
+
+-- Categorias
